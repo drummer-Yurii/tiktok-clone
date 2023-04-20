81b177d705d83944d3108bcb128e643ca9817baa
Yurii Rybachok <drummer2009@ukr.net>
2023-04-20 21:02:13 +0300

    create MenuItemFollow component

--- components/SideNavMain.vue
+++ components/SideNavMain.vue
@@ -10,6 +10,18 @@
             </NuxtLink>
             <MenuItem iconString="Following" colorString="#000000" sizeString="27" />
             <MenuItem iconString="LIVE" colorString="#000000" sizeString="27" />
+
+            <div class="border-b lg:ml-2 mt-2" />
+
+            <div class="lg:block hidden text-xs text-gray-600 font-semibold pt-4 pb-2 px-2">
+                Suggested accounts
+            </div>
+
+            <div class="lg:hidden block pt-3" />
+
+            <div class="cursor-pointer">
+                <MenuItemFollow />
+            </div>
         </div>
     </div>
 </template>

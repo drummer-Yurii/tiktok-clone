89e489bfde46edad9c939abc4273a369c68f8c90
Yurii Rybachok <drummer2009@ukr.net>
2023-04-21 21:18:17 +0300

    create AuthOverlay & Login components

---
+++ components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="text-center text-[28px] mb-4 font-bold">Log in</div>
+
+    <div class="px-6 pb-1.5 text-[15px]">Email address</div>
+
+    <div class="px-6 pb-2">
+
+    </div>
+</template>
+
+<script setup>
+
+</script>

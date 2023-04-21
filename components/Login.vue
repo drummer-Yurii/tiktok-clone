723a8f128768bf03119b7fc70e048fc76d323bd8
Yurii Rybachok <drummer2009@ukr.net>
2023-04-21 21:49:21 +0300

    Login component

--- components/Login.vue
+++ components/Login.vue
@@ -12,8 +12,30 @@
             error=""
         />
     </div>
+
+    <div class="px-6 pb-2">
+        <TextInput 
+            placeholder="Password"
+            v-model:input="password"
+            inputType="password"
+        />
+    </div>
+    <div class="px-6 text-[12px] text-gray-600">Forgot password?</div>
+    
+    <div class="px-6 pb-2 mt-6">
+        <button
+            :disabled="(!email || !password)"
+            :class="(!email || !password) ? 'bg-gray-200' : 'bg-[#F02C56]'"
+            @click="login()"
+            class="w-full text-[17px] font-semibold text-white py-3 rounded-sm"
+        >
+            Log in
+        </button>
+    </div>
 </template>
 
 <script setup>
-
+let email = ref(null)
+let password = ref(null)
+let errors = ref(null)
 </script>

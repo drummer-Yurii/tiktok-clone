f1277b8195d395cd64a8876992b3315ad36e887e
Yurii Rybachok <drummer2009@ukr.net>
2023-04-22 19:28:29 +0300

    axios interceptors

--- stores/general.js
+++ stores/general.js
@@ -1,4 +1,8 @@
-import { defineStore } from "pinia"
+import { defineStore } from "pinia";
+import { useUserStore } from "./user";
+import axios from "../plugins/axios";
+
+const $axios = axios().provide.axios
 
 export const useGeneralStore = defineStore('general', {
     state: () => ({
@@ -12,7 +16,27 @@ export const useGeneralStore = defineStore('general', {
         following: null,
     }),
     actions: {
-        //
+        async hasSessionExpired() {
+            await $axios.interceptors.response.use((response) => {
+                return response;
+            }, (error) => { 
+                switch (error.response.status) {
+                    case 401: // Not logget in
+                    case 419: // Session expired
+                    case 503: // Down for maintenance
+                        // Bounce the user to the login screen with a redirect back
+                        useUserStore().resetUser()
+                        window.location.href = '/';
+                        break;
+                    case 500:
+                        alert('Oops, something went wrong! The team has been notified.');
+                        break;
+                    default:
+                        //Allow individual requests to handle other errors
+                        return Promise.reject(error);    
+                }
+            })
+        }
     },
     persist: true
 })

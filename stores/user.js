13e20c29d9739e1d8c168ccc63ac0879463aa4ca
Yurii Rybachok <drummer2009@ukr.net>
2023-04-22 16:04:06 +0300

    Pinia Login / Register / Logout

--- stores/user.js
+++ stores/user.js
@@ -1,4 +1,7 @@
-import { defineStore } from "pinia"
+import { defineStore } from "pinia";
+import axios from '../plugins/axios';
+
+const $axios = axios().provide.axios
 
 export const useUserStore = defineStore('user', {
     state: () => ({
@@ -8,7 +11,25 @@ export const useUserStore = defineStore('user', {
         image: ''
     }),
     actions: {
-        //
+        async getTokens() {
+            await $axios.get('/sanctum/csrf-cookie')
+        },
+
+        async login(email, password) {
+            await $axios.post('/login', {
+                email: email,
+                password: password
+            })
+        },
+
+        async getUser() {
+            let res = await $axios.get('/api/logged-in-user')
+
+            this.$state.id = res.data[0].id
+            this.$state.name = res.data[0].name
+            this.$state.bio = res.data[0].bio
+            this.$state.image = res.data[0].image
+        },
     },
     persist: true
 })

b6e30177c810937a00d156973591dc68916bcb1b
Yurii Rybachok <drummer2009@ukr.net>
2023-04-24 12:00:14 +0300

    update post page

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -3,14 +3,15 @@
         id="PostPage"
         class="fixed lg:flex justify-between z-50 top-0 left-0 w-full h-full bg-black lg:overflow-hidden overflow-auto"
     >
-        <div class="lg:w-[calc(100%-540px)] h-full relative">
+        <div v-if="$generalStore.selectedPost" class="lg:w-[calc(100%-540px)] h-full relative">
             <NuxtLink
+                :href="$generalStore.isBackUrl"
                 class="absolute z-20 m-5 rounded-full bg-gray-700 p-1.5 hover:bg-gray-800"
             >
                 <Icon name="material-symbols:close" color="#FFFFFF" size="27" />
             </NuxtLink>
 
-           <div v-if="true">
+           <div v-if="($generalStore.ids.length > 1)">
                 <button
                     :disabled="!isLoaded"
                     @click="loopThroughPostsUp()"
@@ -35,9 +36,9 @@
             >
 
             <video 
-                v-if="true"
+                v-if="$generalStore.selectedPost.video"
                 class="absolute object-cover w-full my-auto z-[-1] h-screen"
-                src="/warriot.mp4"
+                :src="$generalStore.selectedPost.video"
             />
 
             <div
@@ -48,46 +49,46 @@
             </div>
             <div class="bg-black bg-opacity-70 lg:min-w-[480px]">
                 <video 
-                    v-if="true"
+                    v-if="$generalStore.selectedPost.video"
                     ref="video"
                     loop
                     muted
                     class="h-screen mx-auto"
-                    src="/Seoul.mp4"
+                    :src="$generalStore.selectedPost.video"
                 />
             </div>
         </div>
 
         <div
             id="InfoSection"
-            v-if="true"
+            v-if="$generalStore.selectedPost"
             class="lg:max-w-[550px] relative w-full h-full bg-white"
         >
             <div class="py-7" />
 
             <div class="flex items-center justify-between px-8">
                 <div class="flex items-center">
-                    <NuxtLink href="/">
+                    <NuxtLink :href="`/profile/${$generalStore.selectedPost.user.id}`">
                         <img 
                             class="rounded-full lg:mx-0 mx-auto"
                             width="40"
-                            src="https://picsum.photos/id/8/300/320"
+                            :src="$generalStore.selectedPost.user.image"
                         >
                     </NuxtLink>
                     <div class="ml-3 pt-0.5">
                         <div class="text-[17px] font-semibold">
-                            User name
+                            {{ $generalStore.allLowerCaseNoCaps($generalStore.selectedPost.user.name) }}
                         </div>
                         <div class="text-[13px] -mt-5 font-light">
-                            User name
+                            {{ $generalStore.selectedPost.user.name }}
                             <span class="relative -top-[2px] text-[30px] pr-0.5">.</span>
-                            <span class="font-medium">Date here</span>
+                            <span class="font-medium">{{ $generalStore.selectedPost.created_at }}</span>
                         </div>
                     </div>
                 </div>
 
                 <icon 
-                    v-if="true"
+                    v-if="$userStore.id === $generalStore.selectedPost.user.id"
                     @click="deletePost()"
                     class="cursor-pointer"
                     name="material-symbols:delete-outline-sharp"
@@ -95,23 +96,27 @@
                 />
             </div>
 
-            <div class="px-8 mt-4 text-sm">This is the post text</div>
+            <div class="px-8 mt-4 text-sm">{{ $generalStore.selectedPost.text }}</div>
 
             <div class="px-8 mt-4 text-sm font-bold">
                 <Icon name="mdi:music" size="17" />
-                original sound - User name
+                original sound - {{ $generalStore.allLowerCaseNoCaps($generalStore.selectedPost.user.name) }}
             </div>
 
             <div class="flex items-center px-8 mt-8">
                 <div class="pb-4 text-center flex items-center">
-                    <button class="rounded-full bg-gray-200 p-2 cursor-pointer">
+                    <button 
+                        @click="isLiked ? unlikePost() : likePost()"
+                        class="rounded-full bg-gray-200 p-2 cursor-pointer"
+                    >
                         <Icon 
                             name="mdi:heart"
                             size="25"
+                            :color="isLiked ? '#F02C56' : ''"
                         />
                     </button>
                     <span class="text-xs pl-2 pr-4 text-gray-800 font-semibold">
-                        123
+                        {{ $generalStore.selectedPost.likes.length }}
                     </span>
                 </div>
 
@@ -135,39 +140,38 @@
                 <div class="pt-2" />
 
                 <div
-                    v-if="false"
+                    v-if="($generalStore.selectedPost.comments.length < 1)"
                     class="text-center mt-6 text-xl text-gray-500"
                 >
                     No comments...
                 </div>
                 <div
                     v-else
+                    v-for="comment in $generalStore.selectedPost.comments"
+                    :key="comment"
                     class="flex items-center justify-between px-8 mt-4"
                 >
                     <div class="flex items-center relative w-full">
-                        <NuxtLink to="/">
+                        <NuxtLink :to="`/profile/${comment.user.id}`">
                             <img 
                                 class="absolute top-0 rounded-full lg:mx-0 mx-auto"
                                 width="40"
-                                src="https://picsum.photos/id/8/300/320"
+                                :src="comment.user.image"
                             >
                         </NuxtLink>
                         <div class="ml-14 pt-0.5 w-full">
                             <div class="text-[18px] font-semibold flex items-center justify-between">
-                                User name
+                                {{ comment.user.name }}
                                 <Icon 
-                                    v-if="true"
-                                    @click="deleteComment()"
+                                    v-if="$userStore.id === comment.user.id"
+                                    @click="deleteComment($generalStore.selectedPost, comment.id)"
                                     class="cursor-pointer"
                                     name="material-symbols:delete-outline-sharp"
                                     size="25"
                                 />
                             </div>
                             <div class="text-[15px] font-light">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                Delectus, quo velit debitis ipsum porro provident voluptatibus 
-                                adipisci minus facere consequuntur alias, ea et autem explicabo 
-                                nulla quos distinctio enim. Placeat!
+                                {{ comment.text }}
                             </div>
                         </div>
                     </div>
@@ -179,7 +183,7 @@
 
             <div
                 id="CreateComment"
-                v-if="true"
+                v-if="$userStore.id"
                 class="absolute flex items-center justify-between bottom-0 bg-white h-[85px] lg:max-w-[550px] w-full py-5 px-8 border-t-2"
             >
                 <div
@@ -209,6 +213,8 @@
 </template>
 
 <script setup>
+const { $generalStore, $userStore, $profileStore } = useNuxtApp()
+
 const route = useRoute()
 const router = useRouter()
 
@@ -217,16 +223,22 @@ let isLoaded = ref(false)
 let comment = ref(null)
 let inputFocused = ref(false)
 
-onMounted(() => {
-    isLoaded.value = true
-    video.value.play()
-    // video.value.addEventListener('loadeddata', (e) => {
-    //     if (e.target) {
-    //         setTimeout(() => {
-    //             isLoaded.value = true
-    //         }, 500)
-    //     }
-    // })
+onMounted(async () => {
+    $generalStore.selectedPost = null
+    try {
+        await $generalStore.getPostById(route.params.id)
+    } catch (error) {
+        if (error && error.response.status === 400) {
+            router.push('/')
+        }
+    }
+    video.value.addEventListener('loadeddata', (e) => {
+        if (e.target) {
+            setTimeout(() => {
+                isLoaded.value = true
+            }, 500)
+        }
+    })
 })
 
 onBeforeUnmount(() => {
